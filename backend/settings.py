636a9479160d3bc095d5dbd4fe521efe55d16e71
Nasr Elsisi <Nasr_Elsisi@outlook.com>
2023-06-21 10:42:26 +0300

    install and create JWT conf

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # --> External Apps <-- #
     "rest_framework",
+    'rest_framework_simplejwt',
     # --> Internal Apps <--#
     "book",
 ]
@@ -125,3 +126,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
