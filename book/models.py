d89176426a935ba4421cbbac7d8211a59dc8fca0
Nasr Elsisi <Nasr_Elsisi@outlook.com>
2023-06-21 10:47:31 +0300

    add page table

--- book/models.py
+++ book/models.py
@@ -25,6 +25,17 @@ class Book(models.Model):
         on_delete=models.CASCADE,
         related_name="books",
     )
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.title
+
+
+class Page(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="pages")
+    number = models.IntegerField()
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Page {self.number} of {self.book.title}"
